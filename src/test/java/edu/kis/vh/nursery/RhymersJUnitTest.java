ad027b3a7d29636f0b90153da871364a585858e1
Jakub-Kubior <255703@edu.p.lodz.pl>
2026-04-10 18:07:42 +0200

    16. Dodanie testów jednostkowych dla klasy FIFORhymer

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -78,4 +78,27 @@ public class RhymersJUnitTest {
         Assert.assertEquals(EMPTY_STACK_VALUE, result);
     }
 
+    @Test
+    public void testFIFOCountOut() {
+        FIFORhymer rhymer = new FIFORhymer();
+        final int EMPTY_STACK_VALUE = -1;
+
+        int result = rhymer.countOut();
+        Assert.assertEquals(EMPTY_STACK_VALUE, result);
+
+        int testValue1 = 4;
+        int testValue2 = 5;
+        rhymer.countIn(testValue1);
+        rhymer.countIn(testValue2);
+
+        result = rhymer.countOut();
+        Assert.assertEquals(testValue1, result);
+
+        result = rhymer.countOut();
+        Assert.assertEquals(testValue2, result);
+
+        result = rhymer.countOut();
+        Assert.assertEquals(EMPTY_STACK_VALUE, result);
+    }
+
 }
